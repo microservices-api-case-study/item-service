80acff13ffaeacbec65ba6a5d41b8fdbca29a33b
arockiaanandraj <arockiaanandraj@gmail.com>
2019-11-26 22:49:40 -0800

    Added displayEnvInfo() to log the environment info

--- src/main/java/com/retail/services/itemservice/ItemServiceController.java
+++ src/main/java/com/retail/services/itemservice/ItemServiceController.java
@@ -2,7 +2,10 @@ package com.retail.services.itemservice;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/service2")
 public class ItemServiceController {
 	
+	private static final Logger log = LoggerFactory.getLogger(ItemServiceController.class);
+	
+	@Autowired
+	Environment environment;
+	
 	@Autowired
 	ItemRepository itemRepository;
 
 	@GetMapping("/items")
 	public List<Item> getAllItems(){
+		log.info(displayEnvInfo());
 		return itemRepository.findAll();
 	}
 	
 	@GetMapping("/items/{itemname}")
 	public Item getItemByName(@PathVariable("itemname") String itemName){
+		log.info(displayEnvInfo());
 		return itemRepository.findByName(itemName);
+	}
+	
+	private String displayEnvInfo() {
+		return "Request received by Item Service running at Port# "+environment.getProperty("local.server.port");
 	}
 }
